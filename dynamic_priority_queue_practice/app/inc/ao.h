1b73c45379989bd93b13b4b2771987ced84f5f38
fchiesad <franco@stamm.bio>
2024-11-25 20:28:35 -0300

    draft

--- dynamic_priority_queue_practice/app/inc/ao.h
+++ dynamic_priority_queue_practice/app/inc/ao.h
@@ -8,12 +8,14 @@
 #ifndef INC_AO_H_
 #define INC_AO_H_
 
-typedef void* event_data_t;
-
-typedef struct {
-	void (*callback_process_event)(ao_event_t*);
-	QueueHandle_t event_queue_h;
-	uint8_t id;
-} ao_t;
-
+/*
+ * Estructura de objeto activo:
+ * 1) callback para ejecutar al recibir un mensaje (se configura al inicio)
+ * 2) queue para recibir mensajes (se configura al inicio y se crea una tarea con ese handle de queue)
+ *
+ * Estructura de mensaje que entra en la queue de RTOS:
+ * 1) estructura de dato con parametros internos (cambia acorde al problema/contexto del momento)
+ * 2) callback para ejecutar en la tarea que recibe el mensaje (posterior a la ejecucion del callback propio del objeto activo target)
+ * 3) estructura de objeto activo target
+ */
 #endif /* INC_AO_H_ */
